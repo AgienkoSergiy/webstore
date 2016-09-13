6219f60b9d08fb582527afa7dc965bc77ab43177
AgienkoSergiy <agienko.sergiy@gmail.com>
2016-09-13 16:56:47 +0300

    Chapter 7 p.186

--- src/main/java/com/packt/webstore/validator/ProductIdValidator.java
+++ src/main/java/com/packt/webstore/validator/ProductIdValidator.java
@@ -5,10 +5,11 @@ import com.packt.webstore.domain.Product;
 import com.packt.webstore.exception.ProductNotFoundException;
 import com.packt.webstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
+@Component
 public class ProductIdValidator implements ConstraintValidator<ProductId, String> {
     @Autowired
     private ProductService productService;
