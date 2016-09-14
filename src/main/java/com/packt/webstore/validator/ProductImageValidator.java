102586ff11ae84ed3ced0dc209ed98580ae864a0
AgienkoSergiy <agienko.sergiy@gmail.com>
2016-09-14 15:15:51 +0300

    Chapter 8 p.217

--- src/main/java/com/packt/webstore/validator/ProductImageValidator.java
+++ src/main/java/com/packt/webstore/validator/ProductImageValidator.java
@@ -8,6 +8,9 @@ import org.springframework.validation.Validator;
 
 @Component
 public class ProductImageValidator implements Validator {
+
+    long allowedSize = 1024000;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return Product.class.isAssignableFrom(aClass);
@@ -15,10 +18,11 @@ public class ProductImageValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        long allowedSize = 1024000;
+
         Product product = (Product) target;
 
-        if(product.getProductImage().getSize()>allowedSize||product.getProductImage()==null) {
+        if((product.getProductImage().getSize()>=allowedSize)||
+                (product.getProductImage().getSize()==0)) {
             errors.rejectValue("productImage",
                     "com.packt.webstore.validator.ProductImageValidator.message");
         }
