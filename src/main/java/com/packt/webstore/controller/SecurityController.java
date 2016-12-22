aced71c89eb8996b1a67ffa69457c98e4e4e0b15
skyrvr <agienko.sergiy@gmail.com>
2016-12-23 00:32:10 +0200

    Testing matrix variables

--- src/main/java/com/packt/webstore/controller/SecurityController.java
+++ src/main/java/com/packt/webstore/controller/SecurityController.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 
-
+@Path(value = "/testing")
 @Controller
 public class SecurityController {
 
@@ -53,6 +57,33 @@ public class SecurityController {
 
         customerService.addCustomer(customer);
         return "redirect:/home"; //TODO improve sign in mechanism (redirect to previous page)
+    }
+
+
+    @RequestMapping(value = "/test")
+    public String getTest(){
+        return "/test";
+    }
+
+
+    @POST
+    public String test(@FormParam("username") String name, @FormParam("password") String pass,
+                       @FormParam("type") String type, @FormParam("age") String age){
+        String request = "/test";
+        if(name!=null){
+            request+=(";name="+name);
+        }
+        if (pass!=null){
+            request+=(";pass="+pass);
+        }
+        if (type!=null){
+            request+=(";type="+type);
+        }
+        if (age!=null){
+            request+=(";age="+age);
+        }
+
+        return request;
     }
 
 }
